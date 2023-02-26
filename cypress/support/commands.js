3856891687958097e682c21ec1d907198c36d764
manali gaikwad <manaligaikwad@manalis-MacBook-Air.local>
2023-02-26 23:57:54 +0000

    Lodgify Challenge

---
+++ cypress/support/commands.js
@@ -0,0 +1,35 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("waitUntilVisible", (locator, timeout = 50000) => {
+  cy.get(locator, { timeout }).should("be.visible");
+});
+
+
+Cypress.Commands.add('selectDate',(locator,numberOfDays)=>{
+cy.get(locator).each(date=>{
+cy.wrap(date).eq(numberOfDays).click()
+})
+})
